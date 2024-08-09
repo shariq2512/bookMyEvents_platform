297f967a34e1948434ac90722552f92291c12e6f
Shariq Khan <shariqkhancse@gmail.com>
2024-08-09 20:46:25 +0100

    fixing stripe endpoint issue

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -9,49 +9,50 @@ import Order from '../database/models/order.model';
 
 export const checkoutOrder = async (order: CheckoutOrderParams) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-    const price = order.isFree ? 0 : Number(order.price) * 100
-
+  
+    const price = order.isFree ? 0 : Number(order.price) * 100;
+  
     try {
-        const session = await stripe.checkout.sessions.create({
-            line_items: [
-              {
-                price_data: {
-                    currency: 'gbp',
-                    unit_amount: price,
-                    product_data: {
-                        name: order.eventTitle
-                    }
-                },
-                quantity: 1
-              },
-            ],
-            metadata: {
-                eventId: order.eventId,
-                buyerId: order.buyerId
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: 'gbp',
+              unit_amount: price,
+              product_data: {
+                name: order.eventTitle
+              }
             },
-            mode: 'payment',
-            success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-            cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-        });
-
-        redirect(session.url!)
+            quantity: 1
+          },
+        ],
+        metadata: {
+          eventId: order.eventId,
+          buyerId: order.buyerId,
+        },
+        mode: 'payment',
+        success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
+        cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+      });
+  
+      redirect(session.url!)
     } catch (error) {
-        throw error
+      throw error;
     }
 }
 
 export const createOrder = async (order: CreateOrderParams) => {
     try {
-        await connectToDatabase()
-
-        const newOrder = await Order.create({
-            ...order,
-            event: order.eventId,
-            buyer: order.buyerId,
-        })
-
-        return JSON.parse(JSON.stringify(newOrder));
+      await connectToDatabase();
+      
+      const newOrder = await Order.create({
+        ...order,
+        event: order.eventId,
+        buyer: order.buyerId,
+      });
+  
+      return JSON.parse(JSON.stringify(newOrder));
     } catch (error) {
-        handleError(error)
+      handleError(error);
     }
 }
